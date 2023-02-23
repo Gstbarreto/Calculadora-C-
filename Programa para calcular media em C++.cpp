4b16a4d6fcb744ef20922c8e30e7704c88428b47
Gstbarreto <106634117+Gstbarreto@users.noreply.github.com>
2023-02-23 16:34:42 -0300

    Add files via upload
    Programa para calcular média

---
+++ Programa para calcular media em C++.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+float a,b,c;
+int main(){
+	
+printf("digite um numero a");
+scanf("%f",&a);
+printf("digite um numero b");
+scanf("%f",&b);
+c=(a+b);
+printf(" a media é= %f",c);
+
+	
+	return 0;
+}
